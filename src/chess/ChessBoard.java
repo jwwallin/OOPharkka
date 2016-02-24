b8421efd3f79ee75a21ec3e02b9ac2ffce944aef
Terppanen <trust552@hotmail.com>
2016-02-24 13:55:53 +0200

    Finish showBoard method

--- src/chess/ChessBoard.java
+++ src/chess/ChessBoard.java
@@ -1,14 +1,33 @@
 package chess;
 
 import chess.pieces.*;
+import chess.types.PieceColour;
+
+import org.fusesource.jansi.AnsiConsole;
+import static org.fusesource.jansi.Ansi.*;
+import static org.fusesource.jansi.Ansi.Color.*;
 
 public class ChessBoard {
-	private Piece[][] board;
+	private Piece[][] board = new Piece[8][8];
+	
 
 	public Piece getPiece(int x, int y) {
 		return board[x][y];
 	}
-	
+
+	public void addPiece(Piece piece, int x, int y){
+		board[x][y] = piece;
+	}
+	public String getId(Piece piece){
+		if (piece instanceof Soldier){
+			return "S";
+		}
+		if (piece instanceof Rook){
+			return "R";
+		}
+		return " ";
+	}
+
 	public boolean move(int[] posOrig, int[] posMoveTo) {
 		//check if position data is valid
 		if (posOrig.length != 2 || posMoveTo.length != 2) return false;
@@ -19,20 +38,20 @@ public class ChessBoard {
 			board[posMoveTo[0]][posMoveTo[1]] = board[posOrig[0]][posOrig[1]];
 			board[posOrig[0]][posOrig[1]] = null;
 			return true;
-			
+
 		} else {
 			//TODO if MoveTo position is not empty
 			return false;
 		}
 	}
-	
+
 	/**
 	 * @return if board drawing is successful
 	 */
 	public boolean drawBoard() {
 		return false;
 	}
-	
+
 	/**
 	 * @return -1, 0, 1 being black player is in check, no check, white player is in check
 	 */
@@ -43,42 +62,50 @@ public class ChessBoard {
 
 	public void showBoard(){
 		boolean white = false;
-		System.out.println("_________________________________"); 					//draw the top horizontal line
-		
+		System.out.println(ansi().reset() + "---------------------------------"); 					//draw the top horizontal line
+
 		for (int i = 0; i < 8; i++){
 			white = !white;															//after each row, change starting color
-			
+
 			if (i > 0){
-				System.out.println("---------------------------------");			//draw horizontal lines in between
+				System.out.println(ansi().reset() + "---------------------------------");			//draw horizontal lines in between
 			}
 
 			for (int j = 0; j < 8; j++){
 
-				System.out.print("|");												//draw a vertical line before each character
+				System.out.print(ansi().reset() + "|");												//draw a vertical line before each character
 
-				if (/*this.*/getPiece(j,i)==null){
-					if (white){
-						System.out.print("   ");									//print a space/blackchar if no pieces at (j,i)
-						white = !white;
-					}else{System.out.print("\u2588\u2588\u2588");
+				if (white){
+					if (getPiece(j,i) != null && getPiece(j,i).getPlayer()==PieceColour.WHITE) {
+						System.out.print(ansi().bg(WHITE).fg(GREEN).a(" " + getId(getPiece(j,i))+ " "));
+					} else {
+
+						System.out.print(ansi().bg(WHITE).fg(RED).a(" " + getId(getPiece(j,i))+ " "));
+
+					}
 					white = !white;
+					
+				}else{
+					if (getPiece(j,i) != null && getPiece(j,i).getPlayer()==PieceColour.WHITE) {
+						System.out.print(ansi().bg(BLACK).fg(GREEN).a(" " + getId(getPiece(j,i))+ " "));
+					} else {
 
+						System.out.print(ansi().bg(BLACK).fg(RED).a(" " + getId(getPiece(j,i))+ " "));
 					}
-				}else{																//if a piece is found, print it in the middle of spaces/blackchars
-					if (white){
-						System.out.print(" " + /*this.*/getPiece(j,i) + " ");
-						white = !white;
-					}else{System.out.println("/u2588" + /*this.*/getPiece(j,i) + "\u2588");
-
-					}
+				white = !white;
+				}
 
 
-					if (j==7){
-						System.out.println("|");									//after the 8th character, draw a vertical line
+				if (j==7){
+					System.out.println(ansi().reset() + "|");									//after the 8th character, draw a vertical line
+					if(i==7){
+						
 					}
 				}
 			}
-			System.out.print( "¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯");					//draw the bottom horizontal line
-		}	
+		}
+		
+		System.out.println(ansi().reset() + "---------------------------------");					//draw the bottom horizontal line
+
 	}
 }
--- src/run/Start.java
+++ src/run/Start.java
@@ -1,6 +1,12 @@
 package run;
 
 import org.fusesource.jansi.AnsiConsole;
+
+import chess.ChessBoard;
+import chess.pieces.Rook;
+import chess.pieces.Soldier;
+import chess.types.PieceColour;
+
 import static org.fusesource.jansi.Ansi.*;
 import static org.fusesource.jansi.Ansi.Color.*;
 
@@ -15,13 +21,19 @@ public class Start {
 	public static void main(String[] args) {
 		AnsiConsole.systemInstall();
 		
-		System.out.println(ansi().bg(WHITE).eraseScreen().fg(RED).a("Hello ").fg(YELLOW).a("World").reset());
+		ChessBoard j = new ChessBoard();
+		Rook R = new Rook(PieceColour.BLACK, null);
+		j.addPiece(R, 2, 2);
+		j.addPiece(new Soldier(PieceColour.WHITE, null), 3, 2);
+		
+		j.showBoard();
 		try {
 			System.in.read();
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
+		
 		AnsiConsole.systemUninstall();
 
 	}
